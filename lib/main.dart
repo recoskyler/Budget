ef8b2e153ada98028572c6d37463d64b5d8d5272
recoskyler <recoskyler224@gmail.com>
2019-12-22 09:35:25 +0200

    edited screen margin

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class SizeConfig {
         safeBlockVertical = (screenHeight -
         _safeAreaVertical) / 100;
 
-        globalInsetPercent = 20;
+        globalInsetPercent = 30;
         buttonTextSize = SizeConfig.safeBlockHorizontal * 4.2;
         regularTextSize = SizeConfig.safeBlockHorizontal * 4.3;
         subTitleTextSize = SizeConfig.safeBlockHorizontal * 4.5;
